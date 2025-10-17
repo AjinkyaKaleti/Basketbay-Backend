4583d80482bb5961a7357a5a2f4c6c8743ddd329
AjinkyaKaleti <ajinkyajc1994@gmail.com>
2025-10-18 01:51:03 +0530

    correction at zepto url

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,15 +1,16 @@
 const { SendMailClient } = require("zeptomail");
 require("dotenv").config();
 
-const url = process.env.ZEPTO_URL;
-const token = process.env.ZEPTO_TOKEN;
-const MAILER_FROM = process.env.MAILER_FROM;
+const ZEPTO_URL = "https://api.zeptomail.in/v1.1/email";
+const ZEPTO_TOKEN =
+  "Zoho-enczapikey wSsVR611/xShWKx+mGeoI+htzAtQAVn2FkR02lKpvn/5G6rEocc7wk3LBQWlHPAXETI4HDIbrbstnB0DgDIH2Yt5mV8EDSiF9mqRe1U4J3x17qnvhDzDV21YlRSIKIoBxAlqk2lnG8sl+g==";
+const MAILER_FROM = "noreply@basketbay.in";
 
-const client = new SendMailClient({ url, token });
+const client = new SendMailClient({ url: ZEPTO_URL, token: ZEPTO_TOKEN });
 
 const sendEmail = async (to, subject, html) => {
   try {
-    await client.sendMail({
+    const response = await client.sendMail({
       from: {
         address: MAILER_FROM,
         name: "BasketBay",
@@ -26,7 +27,7 @@ const sendEmail = async (to, subject, html) => {
       htmlbody: html,
     });
 
-    console.log("Email sent successfully to: ", to);
+    console.log("Email sent successfully to:", to);
     console.log("ZeptoMail Response:", response);
     return true;
   } catch (err) {
